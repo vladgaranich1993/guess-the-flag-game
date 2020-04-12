e19b29cb17b0a1b2078c777a0d95a171e06a92e1
Влад Гаранич <vladislav.garanich@gmail.com>
2020-04-12 18:21:50 +0200

    add lodash library and change structure for main array in the data

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -34,65 +34,63 @@ export default {
       selectedFlag: '',
       currentFlags: [],
       countries: [
-        { id: 1, label: 'Australia', value: 'Australia', continent: 'Australia'},
-        { id: 2, label: 'Austria', value: 'Austria', continent: 'Europe'},
-        { id: 3, label: 'Azerbaijan', value: 'Azerbaijan', continent: 'Asia'},
-        { id: 4, label: 'Albania', value: 'Albania', continent: 'Europe'},
-        { id: 5, label: 'Algeria', value: 'Algeria', continent: 'Africa'},
-        { id: 6, label: 'Angola', value: 'Angola', continent: 'Africa'},
-        { id: 7, label: 'Andorra', value: 'Andorra', continent: 'Europe'},
-        { id: 8, label: 'Antigua and Barbuda', value: 'Antigua and Barbuda', continent: 'America'}
-      ],
-      flags: [
         { 
           id: 1, 
-          url: 'https://cdn.webshopapp.com/shops/94414/files/53448798/australia-flag-icon-free-download.jpg', 
           label: 'Australia', 
-          continent: 'Australia' 
+          value: 'Australia', 
+          continent: 'Australia',
+          url: 'https://cdn.webshopapp.com/shops/94414/files/53448798/australia-flag-icon-free-download.jpg', 
         },
         { 
           id: 2, 
-          url: 'https://cdn.countryflags.com/thumbs/austria/flag-400.png', 
           label: 'Austria', 
-          continent: 'Austria' 
+          value: 'Austria', 
+          continent: 'Europe',
+          url: 'https://cdn.countryflags.com/thumbs/austria/flag-400.png', 
         },
         { 
           id: 3, 
-          url: 'https://cdn.webshopapp.com/shops/94414/files/53480562/azerbaijan-flag-icon-free-download.jpg', 
           label: 'Azerbaijan', 
-          continent: 'Asia' 
+          value: 'Azerbaijan', 
+          continent: 'Asia',
+          url: 'https://cdn.webshopapp.com/shops/94414/files/53480562/azerbaijan-flag-icon-free-download.jpg', 
         },
         { 
           id: 4, 
-          url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Flag_of_Albania.svg/2000px-Flag_of_Albania.svg.png', 
           label: 'Albania', 
-          continent: 'Europe' 
+          value: 'Albania', 
+          continent: 'Europe',
+          url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Flag_of_Albania.svg/2000px-Flag_of_Albania.svg.png', 
         },
-        {
+        { 
           id: 5, 
-          url: 'https://cdn.webshopapp.com/shops/94414/files/53432190/algeria-flag-icon-free-download.jpg', 
           label: 'Algeria', 
-          continent: 'Africa'
+          value: 'Algeria', 
+          continent: 'Africa',
+          url: 'https://cdn.webshopapp.com/shops/94414/files/53432190/algeria-flag-icon-free-download.jpg'
         },
-        {
-          id: 6,
-          url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Flag_of_Angola.svg/900px-Flag_of_Angola.svg.png',
-          label: 'Angola',
-          continent: 'Africa'
+        { 
+          id: 6, 
+          label: 'Angola', 
+          value: 'Angola', 
+          continent: 'Africa',
+          url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Flag_of_Angola.svg/900px-Flag_of_Angola.svg.png'
         },
-        {
-          id: 7,
-          url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Flag_of_Andorra.svg/1280px-Flag_of_Andorra.svg.png',
-          label: 'Andorra',
-          continent: 'Europe'
+        { 
+          id: 7, 
+          label: 'Andorra', 
+          value: 'Andorra', 
+          continent: 'Europe',
+          url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Flag_of_Andorra.svg/1280px-Flag_of_Andorra.svg.png'
         },
-        {
-          id: 8,
-          url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Flag_of_Antigua_and_Barbuda.svg/2000px-Flag_of_Antigua_and_Barbuda.svg.png',
-          label: 'Antigua and Barbuda',
-          continent: 'America'
+        { 
+          id: 8, 
+          label: 'Antigua and Barbuda', 
+          value: 'Antigua and Barbuda', 
+          continent: 'America',
+          url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Flag_of_Antigua_and_Barbuda.svg/2000px-Flag_of_Antigua_and_Barbuda.svg.png'
         }
-      ],
+      ]
     }
   },
   methods: {
@@ -106,13 +104,12 @@ export default {
   },
   created() {
     const idx = Math.floor(Math.random() * this.countries.length);
-    this.selectedFlag = this.flags[idx];
+    this.selectedFlag = this.countries[idx];
 
     this.countries.map(i => {
       this.currentFlags.push(i)
     })
-      
-    console.log(this.currentFlags)
+
   }
 }
 </script>
