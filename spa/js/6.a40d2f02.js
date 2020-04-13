53e3e16be8667c342486090fcb891f09a51e3fea
Влад Гаранич <vladislav.garanich@gmail.com>
2020-04-13 14:45:42 +0200

    remove spa

--- spa/js/6.a40d2f02.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[6],{a1d1:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("\n  About\n")])},l=[],a={},c=a,o=t("2877"),r=Object(o["a"])(c,u,l,!1,null,null,null);e["default"]=r.exports}}]);
